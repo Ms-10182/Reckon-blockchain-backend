2df88ce1292a9e3137dee8a23d2aed278977e2cc
Mayank Sharma <22bcs10182@cuchd.in>
2025-03-08 20:51:49 +0530

    fixed bug

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ app.use(express.json({limit:"16kb"}));
 app.use(urlencoded({ extended: true })); 
 app.use(express.static("public"));
 
-import router from './routes/user.routes.js';
+import UserRouter from './routes/user.routes.js';
 
-app.use("/api/v1/users",router);
+app.use("/api/v1/users",UserRouter);
 
 export {app}
